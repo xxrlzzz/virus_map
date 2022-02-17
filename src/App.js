a56e0ba64fc10d4cbe3942d3e9a8aa82de7bac3d
xxrl <837951112@qq.com>
2022-02-17 18:23:22 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Map,
+  CustomOverlay,
+  InfoWindow,
+  ScaleControl,
+  ZoomControl,
+} from "react-bmapgl";
+import LocData from "./virus-loc.json";
 
+function CustomMarker({ desc }) {
+  return (
+    <div className="CustomMark">
+      <span className="CustomMarkText" title={desc}>
+        中风险
+      </span>
+    </div>
+  );
+}
+
+const center = [120.756455, 31.276098];
 function App() {
+  // console.log(LocData);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        style={{ width: "100%", height: "100%" }}
+        center={{ lng: center[0], lat: center[1] }}
+        zoom="14"
+        maxZoom="17"
+        minZoom="12"
+      >
+        {LocData.map((data, idx) => {
+          const [lng, lat] = data.loc;
+          return (
+            <CustomOverlay position={{ lng, lat }}>
+              <CustomMarker desc={data.desc} />
+            </CustomOverlay>
+          );
+        })}
+
+        {LocData.map((data, idx) => {
+          const [lng, lat] = data.loc;
+          const { desc } = data;
+          return (
+            <InfoWindow
+              key={desc}
+              position={{ lng, lat }}
+              text={desc}
+              title="12"
+            />
+          );
+        })}
+        <InfoWindow
+          position={{ lng: center[0], lat: center[1] }}
+          text="文荟人才公寓"
+        />
+        <ScaleControl />
+        <ZoomControl />
+      </Map>
     </div>
   );
 }
